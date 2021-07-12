aed8da6cd5a1c15361103758965167fea94126a1
Noorvy <81348956+Noorvy@users.noreply.github.com>
2021-07-12 13:19:29 +1000

    Update main.cpp

--- GetInfo/Client/main.cpp
+++ GetInfo/Client/main.cpp
@@ -3,32 +3,33 @@
 #include <set>
 
 #include <zmq.hpp>
-#include "Server_connection.h"
+#include "Connect_to_server.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    const std::set<std::string> command_list{ "GetOsVersion", "GetSystemTime", "GetTickCount", 
-                                      "GlobalMemoryStatus", "GetDriveType", "GetDiskFreeSpace", 
-                                      "GetObjectAcl", "GetObjectOwner", "Exit" };
+    const std::set<std::string> Command_list{ "GetOsVersion", "GetSystemTime", "GetTickCount", 
+                                      "GlobalMemoryStatus", "GetDriveType", "GetDiskFreeSpace",
+                                      "GetObjectOwner", "Exit" };
     zmq::context_t context{ 1 };
-    std::string command;
-
     zmq::socket_t socket{ context, zmq::socket_type::req };
     socket.connect("tcp://localhost:5555");
-    S_connect server_message(socket);
-    server_message.start_list();
+    
+    std::string Command_to_server;
+    Client_process ClientProcess(socket);
+    ClientProcess.help_list();
 
-    while (std::cin >> command) {
-        if (command_list.find(command) != command_list.end()) {
-            if (command == "GetObjectAcl" || command == "GetObjectOwner") {
+    while (std::cin >> Command_to_server) {
+        if (Command_list.find(Command_to_server) != Command_list.end()) {
+            if (Command_to_server == "GetObjectOwner") {
                 std::cout << "Type path to file: (D:/example_path/example_file.txt)" << std::endl;
                 std::string temp_path;
                 std::cin >> temp_path;
-                command.append(temp_path);
+                Command_to_server.append(temp_path);
             }
-            server_message.send_message(command);
+            ClientProcess.send(Command_to_server);
         }
-        else if (command == "Quit") { return 0; }
+        else if (Command_to_server == "Quit") { return 0; }
         else { std::cout << "Wrong command! Repeat please!" << std::endl; }
     }
+    return 0;
 }
